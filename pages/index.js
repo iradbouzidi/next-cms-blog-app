267d4085bb4005bc2201db9e2fbfa196f33b4532
iradbouzidi <b.irad1994@gmail.com>
2023-11-17 02:01:13 +0100

    done

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import { PostCard, Categories, PostWidget } from "../components";
 import { getPosts } from "../services";
+import { FeaturedPosts } from "../sections/index";
 
 export default function Home({ posts }) {
   return (
@@ -8,6 +9,7 @@ export default function Home({ posts }) {
       <Head>
         <title>CMS Blog App</title>
       </Head>
+      <FeaturedPosts />
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
           {posts.map((post) => (
